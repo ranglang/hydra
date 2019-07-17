7c756b54a2994a0025d3daf7a42f070e4c303ec1
Jeff Lewis <jeff-lewis@ip-172-18-6-182.us-west-2.compute.internal>
2019-07-17 09:56:10 -0600

    get upload to minio successfully

--- s3/src/main/scala/hydra/s3/transport/S3Transport.scala
+++ s3/src/main/scala/hydra/s3/transport/S3Transport.scala
@@ -1,16 +1,40 @@
 package hydra.s3.transport
 
 import akka.actor.ActorLogging
+import akka.stream.alpakka.s3.{S3Attributes, S3Settings}
+import akka.stream.alpakka.s3.scaladsl.S3
+import akka.stream.scaladsl.Source
+import akka.stream.{ActorMaterializer, Materializer}
+import akka.util.ByteString
+import com.typesafe.config.ConfigFactory
+import hydra.common.config.ConfigSupport
 import hydra.core.transport.Transport
 import hydra.core.transport.Transport.Deliver
 import hydra.s3.producer.S3Record
 
-class S3Transport extends Transport with ActorLogging {
+import scala.concurrent.ExecutionContext
+import scala.util.{Failure, Success}
+
+class S3Transport extends Transport with ActorLogging with ConfigSupport {
+
+  private implicit val executionContext: ExecutionContext = context.dispatcher
+  private implicit val materializer: Materializer = ActorMaterializer()
 
   override def transport: Receive = {
     case Deliver(record: S3Record, deliveryId, callback) =>
+      val b = ConfigFactory.load.getString("alpakka.s3.endpoint-url")
+      val s3Sink = S3.multipartUpload(record.destination, record.fileName)
+      val result = Source.single(ByteString(record.payload))
+        .withAttributes(S3Attributes.settingsPath("alpakka.s3"))
+        .runWith(s3Sink)
 
-      callback.onCompletion(deliveryId, None, None)
+      result.onComplete {
+        case Success(_) =>
+          callback.onCompletion(deliveryId, None, None)
+        case Failure(e) =>
+          log.error("Failed to upload to S3, {}", e)
+          callback.onCompletion(deliveryId, None, Some(e))
+      }
   }
 
 }
